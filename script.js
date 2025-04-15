24d5a44eea2df5916ec927449b43e62b79278453
psalmkheed <psalmkheed@gmail.com>
2025-04-15 17:37:27 +0100

    Adding of files

---
+++ script.js
@@ -0,0 +1,12 @@
+$(window).on("load", function () {
+  // // Load external HTML files from pages folder
+  $("#header").load("pages/header.html");
+
+  $("#heroSection").load("pages/hero-section.html");
+
+  $("#menu").load("pages/menu.html");
+
+  $("#about-us").load("pages/about.html");
+
+  $("#blog").load("pages/blog.html");
+});
